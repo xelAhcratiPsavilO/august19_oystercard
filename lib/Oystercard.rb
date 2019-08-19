dbcd078b07c7d6291d3bbebad9bb8b5e0b1e2b00
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-08-19 18:21:43 +0100

    Test rspec setup

---
+++ lib/Oystercard.rb
@@ -0,0 +1,7 @@
+class Oystercard
+
+  def check_tests
+    return "Test Infraestructure working"
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,13 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  it 'checks the tests infraestructure' do
+    expect(subject.check_tests).to eq("Test Infraestructure working")
+  end
+
+  it 'checks the tests infraestructure' do
+    expect(subject.check_tests).to_not eq("est Infraestructure working")
+  end
 
 end
