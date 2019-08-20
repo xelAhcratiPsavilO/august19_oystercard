b55a50553d2c5d2087d59faba409630e205286c9
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-08-20 20:35:53 +0100

    Add ability to top up oystercard

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -6,4 +6,8 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(credit)
+    @balance += credit
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,18 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'has balance' do
-    expect(subject.balance).to eq(0)
+  subject(:oystercard) { described_class.new }
+
+  describe '#balance' do
+    it 'reads current balance' do
+      expect(oystercard.balance).to eq(0)
+    end
+  end
+
+  describe '#top_up' do
+    it 'adds credit to the card' do
+      expect{ oystercard.top_up 30 }.to change { oystercard.balance }.by 30
+    end
   end
 
 end
