9c10ff5e95ca321ccc70c18f3248c96cd358ccd6
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-08-22 06:22:03 +0100

    Create zone class

---
+++ spec/station_spec.rb
@@ -0,0 +1,22 @@
+require 'station'
+
+describe Station do
+
+
+  subject(:station) { described_class.new(:name, :zone) }
+  let(:name){ double :name }
+  let(:zone){ double :zone }
+
+  describe '#name' do
+    it 'responds with the initialized name' do
+      expect(station.name).to eq :name
+    end
+  end
+
+  describe '#zone' do
+    it 'responds with the initialized zone' do
+      expect(station.zone).to eq :zone
+    end
+  end
+
+end
